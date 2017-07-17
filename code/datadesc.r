611d40ff7a33dc6beb1c84f16d72d9e3f6aaa761
Pratik Gupte <pratikgupte16@gmail.com>
2017-07-17 14:26:19 +0200

    add improved paper version

--- code/datadesc.r
+++ code/datadesc.r
@@ -4,15 +4,24 @@ source("dataprep.R")
 
 geese$year = year(geese$time)
 
-goose_year_month = data.frame(table(geese %>% dplyr::select(year, month)))
+geese$month = substr(months(geese$time), 1, 3)
+
+geese$monthn = month(geese$time)
+winter.month = c("Aug", "Sep", "Oct",
+                 "Nov","Dec","Jan","Feb",
+                 "Mar", "Apr")
+
+geese$month = ordered(geese$month, winter.month)
+
+goose_year_month = data.frame(table(geese %>% dplyr::select(Breeding_year, month)))
 
 library(reshape2)
-goose_year_month = melt(goose_year_month, id.vars = c("year","month"))
+goose_year_month = melt(goose_year_month, id.vars = c("Breeding_year","month"))
 
 pdf(file = "~/git/thesis/texts/data_density.pdf", height = 2, width = 5,
     title = "data_density")
 ggplot(data = goose_year_month,
-       aes(x=year, y=month, fill=value)) + 
+       aes(x=Breeding_year, y=month, fill=value)) + 
   geom_tile()+ 
   scale_fill_gradient2(low = "#fc8d59", mid = "#ffffbf", 
                        high = "#1a9850",
@@ -24,22 +33,24 @@ dev.off()
 
 ####Data density geese org####
 geeseorg$year = year(geeseorg$date)
-geeseorg$month = month(geeseorg$date)
+geeseorg$month = substr(months(geeseorg$date), 1,3)
 
-gorg_year_month = data.frame(table(geeseorg %>% dplyr::select(year, month)))
+geeseorg$month = ordered(geeseorg$month, winter.month)
+
+gorg_year_month = data.frame(table(geeseorg %>% dplyr::select(breedyr, month)))
 
 library(reshape2)
-gorg_year_month = melt(gorg_year_month, id.vars = c("year","month"))
+gorg_year_month = melt(gorg_year_month, id.vars = c("breedyr","month"))
 
 pdf(file = "~/git/thesis/texts/data_density_gorg.pdf", height = 2, width = 5,
     title = "data_density_gorg")
 ggplot(data = gorg_year_month,
-       aes(x=year, y=month, fill=value)) + 
+       aes(x=breedyr, y=month, fill=value)) + 
   geom_tile()+ 
   scale_fill_gradient2(low = "#fc8d59", mid = "#ffffbf", 
                        high = "#1a9850",
                        midpoint = 49)+
-  labs(list(fill = "Records", y = "Month", x = "Year"))+
+  labs(list(fill = "Records", y = "Month", x = "Breeding year"))+
   theme_bw()+
   theme(axis.text.x=element_text(angle=45, hjust = 1))
 dev.off()
